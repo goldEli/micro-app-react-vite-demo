b9d675485e00b084cafbe443a69e21941ca4b71d
miaoyu <miaoyu@ifun.com>
2023-04-10 09:58:42 +0800

    feat: upgrade vite 4.x

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # test micro app
 
 基座和子引用都采用 vite
+
+```
+http://localhost:3000/my-page
+```
--- child/vite.config.js
+++ child/vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { join } from 'path'
-import { writeFileSync } from 'fs'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { join } from "path";
+import { writeFileSync } from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `${process.env.NODE_ENV === 'production' ? 'http://my-site.com' : ''}/appChild/`,
-  plugins: [react(),
+  server: {
+    port: 3001,
+  },
+  base: `${
+    process.env.NODE_ENV === "production" ? "http://my-site.com" : ""
+  }/appChild/`,
+  plugins: [
+    react(),
     (function () {
-      let basePath = ''
+      let basePath = "";
       return {
         name: "app-child",
         apply: "build",
@@ -18,13 +24,20 @@ export default defineConfig({
         },
         renderChunk(code, chunk) {
           // build后，import会通过相对地址引入模块，需要将其补全
-          if (chunk.fileName.endsWith(".js") && /(from|import)(\s*['"])(\.\.?\/)/g.test(code)) {
-            code = code.replace(/(from|import)(\s*['"])(\.\.?\/)/g, (all, $1, $2, $3) => {
-              return all.replace($3, new URL($3, basePath).href);
-            });
+          if (
+            chunk.fileName.endsWith(".js") &&
+            /(from|import)(\s*['"])(\.\.?\/)/g.test(code)
+          ) {
+            code = code.replace(
+              /(from|import)(\s*['"])(\.\.?\/)/g,
+              (all, $1, $2, $3) => {
+                return all.replace($3, new URL($3, basePath).href);
+              }
+            );
           }
           return code;
         },
       };
-    })(),]
-})
+    })(),
+  ],
+});
